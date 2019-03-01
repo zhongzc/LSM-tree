0252074ead48ddd77c923e178c6a154c7b911ee9
tangenta <tangenta@126.com>
2019-03-01 16:28:03 +0800

    refactor build.sbt

---
+++ src/test/scala/com/gaufoo/sst/SSTEngineTest.scala
@@ -0,0 +1,10 @@
+package com.gaufoo.sst
+
+import org.scalatest.FlatSpec
+
+class SSTEngineTest extends FlatSpec {
+  "Hello" should "start with H" in {
+    "test".
+    assert("Hello".startsWith("H"))
+  }
+}
