b810e7fdab49f60de1b6c5a39e5c0c673c14dab0
tangenta <tangenta@126.com>
2019-03-03 21:51:08 +0800

    refactor test/, add more test

--- src/test/scala/com/gaufoo/utils/bloomFilter/BloomFilterTest.scala
+++ src/test/scala/com/gaufoo/utils/bloomFilter/BloomFilterTest.scala
@@ -1,21 +1,10 @@
 package com.gaufoo.utils.bloomFilter
 
+import com.gaufoo.BasicAsyncFlatSpec
 import com.gaufoo.utils.bloomfilter.SimpleBloomFilter
-import org.scalatest.{FlatSpec, Matchers}
-import org.slf4s.Logging
 
-import scala.collection.mutable
-import scala.util.Random
-
-class BloomFilterTest extends FlatSpec with Logging with Matchers {
-  private def randomString(maxLength: Int): String = {
-    val l = Random.nextInt(maxLength + 1)
-    val result = new mutable.StringBuilder()
-    for (_ <- 1 to l) {
-      result.append(Random.nextPrintableChar())
-    }
-    result.toString()
-  }
+class BloomFilterTest extends BasicAsyncFlatSpec {
+  import com.gaufoo.benchmark.utils.FakeDataGenerator.randomString
 
   "BloomFilter" should "be able to work" in {
     val maxLength = 50
@@ -41,6 +30,6 @@ class BloomFilterTest extends FlatSpec with Logging with Matchers {
 
     log.debug(s"Theoretical false positive rate: ${bloomFilter.falsePositiveProbability}")
     log.debug(s"Actual      false positive rate: ${falsePositive * 1.0 / total}")
-
+    succeed
   }
 }
